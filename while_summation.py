140e7959895fd627f8f55a71a8a8643fc4868fc6
LincolnDabb3 <dabblin000@alpinesd.org>
2024-10-22 10:16:26 -0600

    Update while_summation.py

--- while_summation.py
+++ while_summation.py
@@ -1,1 +1,8 @@
-#while summation code following directions from Readme
+num = int(input())
+product = 0
+the_end = 0
+
+while the_end < num:
+    product += the_end
+    the_end += 1
+print(product + num)
